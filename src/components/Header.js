faa330c1b80128773cf6d9ced9a284ab48ba1c8c
AE06909 <wes.gonzales@elevancehealth.com>
2024-03-14 14:48:14 -0400

    Themes complete.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,32 @@
 import Logo from "../assets/logo.png"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Header = () => {
-  return (
-    <header>
-		<div className='logo'>
-			<img src={Logo} alt="task manager logo" />
-			<span>Taskmate</span>
-		</div>
+	const [theme, setTheme] = useState(JSON.parse(localStorage.getItem("theme")) || "medium")
 
-		<div className="themeSelector">
-			<span className="light activeTheme"></span>
-			<span className="medium"></span>
-			<span className="dark"></span>
-			<span className="gOne"></span>
-			<span className="gTwo"></span>
-			<span className="gThree"></span>
-		</div>
-    </header>
-  )
+	useEffect(() => {
+		localStorage.setItem("theme", JSON.stringify(theme))
+		document.documentElement.removeAttribute("class")
+		document.documentElement.classList.add(theme)
+	},[theme])
+
+	return (
+		<header>
+			<div className='logo'>
+				<img src={Logo} alt="task manager logo" />
+				<span>Taskmate</span>
+			</div>
+
+			<div className="themeSelector">
+				<span onClick={() => setTheme("light")} className={theme === "light" ? "light activeTheme" : "light"}></span>
+				<span onClick={() => setTheme("medium")} className={theme === "medium" ? "medium activeTheme" : "medium"}></span>
+				<span onClick={() => setTheme("dark")} className={theme === "dark" ? "dark activeTheme" : "dark"}></span>
+				<span onClick={() => setTheme("gOne")} className={theme === "gOne" ? "gOne activeTheme" : "gOne"}></span>
+				<span onClick={() => setTheme("gTwo")} className={theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}></span>
+				<span onClick={() => setTheme("gThree")} className={theme === "gThree" ? "gThree activeTheme" : "gThree"}></span>
+			</div>
+		</header>
+	)
 }
 
 export default Header
